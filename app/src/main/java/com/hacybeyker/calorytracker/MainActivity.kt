dba1be40cc359ff38f2b44d313ef256f0a2e64bd
Carlos Osorio <hacybeyker@gmail.com>
2024-02-23 20:25:34 -0500

    age screen

--- app/src/main/java/com/hacybeyker/calorytracker/MainActivity.kt
+++ app/src/main/java/com/hacybeyker/calorytracker/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.hacybeyker.calorytracker
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.hacybeyker.calorytracker.navigation.navigate
 import com.hacybeyker.calorytracker.ui.theme.CaloryTrackerTheme
 import com.hacybeyker.core.navigation.Route
+import com.hacybeyker.onboarding_presentation.age.AgeScreen
 import com.hacybeyker.onboarding_presentation.gender.GenderScreen
 import com.hacybeyker.onboarding_presentation.welcome.WelcomeScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,42 +26,51 @@ class MainActivity : ComponentActivity() {
         setContent {
             CaloryTrackerTheme {
                 val navController = rememberNavController()
-                NavHost(
-                    navController = navController,
-                    startDestination = Route.WELCOME
-                ) {
-                    composable(Route.WELCOME) {
-                        WelcomeScreen(onNavigate = navController::navigate)
-                    }
-                    composable(Route.AGE) {
+                val scaffoldState = rememberScaffoldState()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    scaffoldState = scaffoldState,
+                ) { paddingValues ->
+                    Log.d("TAG", "Here - onCreate: $paddingValues")
+                    NavHost(
+                        navController = navController,
+                        startDestination = Route.WELCOME
+                    ) {
+                        composable(Route.WELCOME) {
+                            WelcomeScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.AGE) {
+                            AgeScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate)
+                        }
+                        composable(Route.GENDER) {
+                            GenderScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.HEIGHT) {
 
-                    }
-                    composable(Route.GENDER) {
-                        GenderScreen(onNavigate = navController::navigate)
-                    }
-                    composable(Route.HEIGHT) {
+                        }
+                        composable(Route.WEIGHT) {
 
-                    }
-                    composable(Route.WEIGHT) {
+                        }
+                        composable(Route.NUTRIENT_GOAL) {
 
-                    }
-                    composable(Route.NUTRIENT_GOAL) {
+                        }
+                        composable(Route.ACTIVITY) {
 
-                    }
-                    composable(Route.ACTIVITY) {
+                        }
+                        composable(Route.GOAL) {
 
-                    }
-                    composable(Route.GOAL) {
+                        }
+                        composable(Route.TRACKER_OVERVIEW) {
 
-                    }
-                    composable(Route.TRACKER_OVERVIEW) {
+                        }
+                        composable(Route.SEARCH) {
 
+                        }
                     }
-                    composable(Route.SEARCH) {
-
-                    }
-
                 }
+
             }
         }
     }
