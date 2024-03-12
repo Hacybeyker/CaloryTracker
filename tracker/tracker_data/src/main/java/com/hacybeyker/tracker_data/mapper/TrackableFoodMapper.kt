6b91ffd43816fb536f69846ff4f5351cff8cb82c
Carlos Osorio <hacybeyker@gmail.com>
2024-03-11 21:39:31 -0500

    setup localDB

---
+++ tracker/tracker_data/src/main/java/com/hacybeyker/tracker_data/mapper/TrackableFoodMapper.kt
@@ -0,0 +1,22 @@
+package com.hacybeyker.tracker_data.mapper
+
+import com.hacybeyker.tracker_data.remote.dto.Product
+import com.hacybeyker.tracker_domain.model.TrackableFood
+import kotlin.math.roundToInt
+
+fun Product.toTrackableFood(): TrackableFood? {
+
+    val caloriesPer100g: Int = nutriments.energyKcal100g.roundToInt()
+    val carbsPer100g = nutriments.carbohydrates100g.roundToInt()
+    val proteinPer100g = nutriments.proteins100g.roundToInt()
+    val fatPer100g = nutriments.fat100g.roundToInt()
+
+    return TrackableFood(
+        name = productName ?: return null,
+        imageUrl = imageFrontThumbUrl,
+        caloriesPer100g = caloriesPer100g,
+        carbsPer100g = carbsPer100g,
+        proteinPer100g = proteinPer100g,
+        fatPer100g = fatPer100g
+    )
+}
